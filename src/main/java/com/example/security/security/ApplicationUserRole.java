b14b89957ffac3d84238e4360f008a850362b6c7
Cosinus <cheikh.s@makeba.money>
2021-08-09 12:41:46 +0000

    Role Based Authentication

--- src/main/java/com/example/security/security/ApplicationUserRole.java
+++ src/main/java/com/example/security/security/ApplicationUserRole.java
@@ -1,8 +1,11 @@
 package com.example.security.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.example.security.security.ApplicationUserPermission.*;
 
@@ -19,6 +22,15 @@ public enum ApplicationUserRole {
     }
 
     public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<GrantedAuthority> getGrantedAuthorities() {
+        Set<GrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
         return permissions;
     }
 }
